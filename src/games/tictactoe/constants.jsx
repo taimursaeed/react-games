d5f718960f1eb4f51cce942244138ca632315731
taimursaeed <taimur.saeed.malik@gmail.com>
2021-06-24 12:12:30 +0500

    update icon

--- src/games/tictactoe/constants.jsx
+++ src/games/tictactoe/constants.jsx
@@ -6,16 +6,16 @@ const TURNTYPE = {
     symbol: (
       <svg
         width="1em"
-        className="w-6 h-6"
-        fill="currentColor"
-        viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       >
-        <path
-          fillRule="evenodd"
-          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-          clipRule="evenodd"
-        ></path>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <circle cx="12" cy="12" r="9"></circle>
       </svg>
     ),
   },
@@ -25,7 +25,6 @@ const TURNTYPE = {
     symbol: (
       <svg
         width="1em"
-        className="w-6 h-6"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
