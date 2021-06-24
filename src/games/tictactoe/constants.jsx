0297808c2bd75c49e42128eb2e5ad4995c056d5b
taimursaeed <taimur.saeed.malik@gmail.com>
2021-06-24 12:40:09 +0500

    fix svg syntax error

--- src/games/tictactoe/constants.jsx
+++ src/games/tictactoe/constants.jsx
@@ -8,11 +8,11 @@ const TURNTYPE = {
         width="1em"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        stroke-width="3"
+        strokeWidth="3"
         stroke="currentColor"
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <circle cx="12" cy="12" r="9"></circle>
@@ -27,11 +27,11 @@ const TURNTYPE = {
         width="1em"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        stroke-width="3"
+        strokeWidth="3"
         stroke="currentColor"
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <line x1="18" y1="6" x2="6" y2="18"></line>
